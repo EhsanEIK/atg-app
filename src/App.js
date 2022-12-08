a262e6526d5f2f9e4bd23243fe2d9d5ed0f5769b
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-08 17:52:40 +0600

    added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import Menubar from './Pages/Shared/Menubar/Menubar';
+import router from './Routes/Routes/routes';
 
 function App() {
   return (
     <div>
-      <Menubar></Menubar>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
