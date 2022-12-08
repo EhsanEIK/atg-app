0a51beffcf76c33941b43b4104c562d836a5cd6e
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-12-08 18:27:32 +0600

    added text in TopHeader

--- src/components/TopHeader/TopHeader.js
+++ src/components/TopHeader/TopHeader.js
@@ -3,7 +3,11 @@ import './TopHeader.css';
 
 const TopHeader = () => {
     return (
-        <div className='banner'>
+        <div className='banner positon-relative'>
+            <div className='position-absolute' style={{ top: "386px", left: "200px" }}>
+                <h3 style={{ fontWeight: "700", fontSize: "36px" }}>Computer Engineering</h3>
+                <p style={{ fontWeight: "400", fontSize: "18px" }}>142,765 Computer Engineers follow this</p>
+            </div>
         </div>
     );
 };
